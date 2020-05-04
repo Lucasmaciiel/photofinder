14d3b27530b2a6df69414895ba984f20f346851f
Lucas Maciel <lucasmacielgois@gmail.com>
2020-05-04 18:07:23 -0300

    componetizando mensagens de validacao

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { SignInComponent } from './sigin/signin.component';
 import { ReactiveFormsModule  } from '@angular/forms'
 import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [SignInComponent],
    imports: [
        ReactiveFormsModule,
-       CommonModule]
+       CommonModule,
+       VMessageModule 
+    ]
 })
 export class homeModule {}
 
---
+++ alurapic/src/app/shared/components/vmessage/vmessage.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'ap-message',
+    templateUrl: './vmessage.component.html'
+})
+export class VMessageComponent {
+
+    @Input() text: '';
+
+
+
+
+
+}
