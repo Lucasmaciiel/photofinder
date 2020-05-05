1372cd02035576bbc2a71e6dd954e10f977fd74e
Lucas Maciel <lucasmacielgois@gmail.com>
2020-05-05 20:04:39 -0300

    redirecionamento apos login

--- alurapic/src/app/home/home.module.ts
+++ alurapic/src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import { SignInComponent } from './sigin/signin.component';
 import { ReactiveFormsModule  } from '@angular/forms'
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [SignInComponent],
    imports: [
        ReactiveFormsModule,
        CommonModule,
-       VMessageModule 
+       VMessageModule,
+       RouterModule
     ]
 })
 export class homeModule {}
--- alurapic/src/app/home/sigin/signin.component.ts
+++ alurapic/src/app/home/sigin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/auth.service';
+import { Router } from '@angular/router';
 
 @Component( {
     templateUrl: './signin.component.html'
@@ -10,7 +11,8 @@ export class SignInComponent implements OnInit{
     loginForm: FormGroup;
     
     constructor(private formBuilder: FormBuilder,
-                private authService: AuthService){}
+                private authService: AuthService,
+                private router: Router){}
 
     ngOnInit(): void {
         this.loginForm = this.formBuilder.group({
@@ -27,7 +29,7 @@ login() {
     this.authService
         .authenticate(userName, password)
         .subscribe(
-            () => console.log('autenticado'),
+            () => this.router.navigate(['user', userName]), 
             err => {
                 console.log(err);
                 this.loginForm.reset();
