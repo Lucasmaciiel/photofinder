213dab6f4a6af1f8be35b6018adc8d2011aa4a15
Lucas Maciel <lucasmacielgois@gmail.com>
2020-05-17 15:05:04 -0300

    Async pipe

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations:[HeaderComponent],
-    exports: [HeaderComponent]
+    exports: [HeaderComponent],
+    imports: [CommonModule]
 })
 export class CoreModule {
 
--- alurapic/src/app/core/header/header.component.ts
+++ alurapic/src/app/core/header/header.component.ts
@@ -11,11 +11,8 @@ import { User } from '../user/user';
 export class HeaderComponent {
 
     user$: Observable<User>;
-    user: User
-
 
         constructor(userService: UserService){
             this.user$ = userService.getUser();
-            this.user$.subscribe(user => this.user = user)
         }
 }
