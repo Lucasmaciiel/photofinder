cdc073ee36c9f35191cdd56f0fd2eee842cd1fec
Lucas Maciel <lucasmacielgois@gmail.com>
2020-05-17 10:04:14 -0300

    separação de responsabilidades

--- alurapic/src/app/core/auth/auth.service.ts
+++ alurapic/src/app/core/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
-import { TokenService } from '../token/token.service';
+import { UserService } from '../user/user.service';
 
 const API_URL = 'http://localhost:3000';
 
@@ -11,13 +11,13 @@ const API_URL = 'http://localhost:3000';
 export class AuthService {
 
   constructor(private http: HttpClient,
-              private tokenService: TokenService) { }
+              private userService: UserService) { }
 
   authenticate(userName: string, password: string) {
     return this.http.post(API_URL + '/user/login', {userName, password}, { observe: 'response'})
     .pipe(tap( res => {
       const authToken = res.headers.get('x-access-token');
-      this.tokenService.setToken(authToken);
+      this.userService.setToken(authToken);
      // window.localStorage.setItem('authToken', authToken);
       console.log(`User ${userName} authenticated with token ${authToken}`)
     }))
