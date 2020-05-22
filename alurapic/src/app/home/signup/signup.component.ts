63b4e13ec114709ce7c9daa64ea0518b4a84987d
Lucas Maciel <lucasmacielgois@gmail.com>
2020-05-22 18:15:11 -0300

    validacao do formulario

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,8 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signup.component.html'
 })
-export class SigUpComponent{
+export class SigUpComponent implements OnInit{
 
+signupForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder){
+    }
+    
+    ngOnInit(): void {
+        
+        this.signupForm = this.formBuilder.group({
+            email: ['', 
+            [ 
+                Validators.required, 
+                Validators.email
+            ]],
+            userName: ['', 
+            [ 
+                Validators.required, 
+                Validators.minLength(2),
+                Validators.maxLength(40)
+            ]],
+            fullName: ['', 
+            [ 
+                Validators.required,
+                Validators.pattern(/^[a-z0-9_\-]+$/),
+                Validators.minLength(2),
+                Validators.maxLength(30)
+            ]],
+            password: ['', 
+            [
+                Validators.required,
+                Validators.minLength(8),
+                Validators.maxLength(14)
+            ]]
+        });
+    
+    }
+ 
 }
