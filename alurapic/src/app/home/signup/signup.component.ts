06a0ae447c6dca09fd20b80f3ee98f5a7b9488a6
Lucas Maciel <lucasmacielgois@gmail.com>
2020-06-14 17:50:45 -0300

    Criando meu proprio validador

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -28,7 +29,7 @@ signupForm: FormGroup;
             fullName: ['', 
             [ 
                 Validators.required,
-                Validators.pattern(/^[a-z0-9_\-]+$/),
+                lowerCaseValidator,
                 Validators.minLength(2),
                 Validators.maxLength(30)
             ]],
