e7b932419918a83def802653ef154977bec95c2e
Lucas Maciel <lucasmacielgois@gmail.com>
2020-07-08 08:22:25 -0300

    Removendo o node Modules do git

--- alurapic/src/app/home/signup/user-not-taken.validator.services.ts
+++ alurapic/src/app/home/signup/user-not-taken.validator.services.ts
@@ -1,23 +1,26 @@
-import { Injectable } from '@angular/core';
-import { SignUpService } from './signup.service';
-import { controlNameBinding } from '@angular/forms/src/directives/reactive_directives/form_control_name';
+import { debounceTime, switchMap, first, map } from 'rxjs/operators';
 import { AbstractControl } from '@angular/forms';
-import { debounceTime, switchMap, map, first } from 'rxjs/operators'
+import { SignUpService } from './signup.service';
+import { Injectable } from '@angular/core';
 
-@Injectable( { providedIn: 'root'})
-export class UserNotTakenValidatorService{
+@Injectable({ providedIn: 'root' })
+export class UserNotTakenValidatorService {
 
-    constructor(private signupService: SignUpService){}
+    constructor(private signUpService: SignUpService) {}
 
-    checkUserNameTaken(){
+    checkUserNameTaken() {
+
         return (control: AbstractControl) => {
-            return control.valueChanges
-                   .pipe(debounceTime(300))
-                   .pipe(switchMap(userName =>
-                        this.signupService.checkUserNameTaken(userName)
+            return (control: AbstractControl) => {
+                return control
+                    .valueChanges
+                    .pipe(debounceTime(300))
+                    .pipe(switchMap(userName =>
+                        this.signUpService.checkUserNameTaken(userName)
                     ))
-                    .pipe(map(istaken => istaken ? {userNameTaken: true} : null))
+                    .pipe(map(isTaken => isTaken ? { userNameTaken: true } : null))
                     .pipe(first());
-        }
+              }    
+             }
     }
 }
