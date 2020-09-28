0478575ca0b147957745b197f76fae8d896a313c
Lucas Maciel <lucasmacielgois@gmail.com>
2020-09-28 17:25:13 -0300

    Interceptadores e envio do token

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
     declarations:[HeaderComponent],
     exports: [HeaderComponent],
     imports: [
         CommonModule,
-        RouterModule]
+        RouterModule
+    ],
+    providers: [ {
+        provide: HTTP_INTERCEPTORS,
+        useClass: RequestInterceptor,
+        multi: true 
+    }
+
+    ]
 })
 export class CoreModule {
 
