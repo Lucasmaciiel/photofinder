02d6223e8085e8b6a74bb8b50e91dbd2323115a2
Lucas Maciel <lucasmacielgois@gmail.com>
2021-03-09 09:26:15 -0300

    Formulário upload de fotos

--- alurapic/src/app/photos/photo-form/photo-form.module.ts
+++ alurapic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotoFormComponent } from './photo-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
   declarations: [ PhotoFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    VMessageModule,
+    FormsModule
   ]
 })
 export class PhotoFormModule { }
