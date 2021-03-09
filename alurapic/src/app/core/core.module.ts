15e18057d3ebc3a8121338d8013e9263c372288c
Lucas Maciel <lucasmacielgois@gmail.com>
2021-03-09 09:15:44 -0300

    Rodapé

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -4,10 +4,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RequestInterceptor } from './auth/request.interceptor';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations:[HeaderComponent],
-    exports: [HeaderComponent],
+    declarations:[
+        HeaderComponent,
+        FooterComponent],
+    exports: [
+        HeaderComponent,
+        FooterComponent],
     imports: [
         CommonModule,
         RouterModule
