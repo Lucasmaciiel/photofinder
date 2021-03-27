02101344707ee3ff0f19bdc1289dedaf5e7ff52e
Lucas Maciel <lucasmacielgois@gmail.com>
2021-03-27 15:49:59 -0300

    Comentário de fotos

--- alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ alurapic/src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -26,4 +26,15 @@ export class PhotoCommentsComponent implements OnInit{
 
     }
 
+    save(){
+        const comment = this.commentForm.get('comment').value as string;
+        this.photoService
+            .addComment(this.photoId, comment)
+            .subscribe(() => {
+                this.commentForm.reset();
+                alert('Comentário adicionado com sucesso')
+            })
+        
+    }
+
 }
