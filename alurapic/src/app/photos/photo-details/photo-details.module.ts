15cb7ddf25456ff8642ffb02f0568903491b5da4
Lucas Maciel <lucasmacielgois@gmail.com>
2021-04-04 19:50:03 -0300

    Exibe elemento curtir apenas quando logado

--- alurapic/src/app/photos/photo-details/photo-details.module.ts
+++ alurapic/src/app/photos/photo-details/photo-details.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
 import { VMessageModule } from "src/app/shared/components/vmessage/vmessage.module";
 import { PhotoOwnerOnlyDirective } from "./photo-owner-only/photo-owner-only.directive";
+import { showIfLoggedModule } from "src/app/shared/directives/show-if-logged/show-if-logged.module";
 
 @NgModule({
     declarations: [
@@ -22,7 +23,8 @@ import { PhotoOwnerOnlyDirective } from "./photo-owner-only/photo-owner-only.dir
         PhotoModule,
         RouterModule,
         ReactiveFormsModule,
-        VMessageModule
+        VMessageModule,
+        showIfLoggedModule
         ]
 })
 export class PhotoDetailsModule { }
